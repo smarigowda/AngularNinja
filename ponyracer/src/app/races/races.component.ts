e7f869c1b3bf98503f29d7e0df2d64a5f7ffebfc
Santosh Marigowda <santosh.marigowda2@sky.uk>
2021-05-02 19:41:46 +0100

    refefencing a template

--- ponyracer/src/app/races/races.component.ts
+++ ponyracer/src/app/races/races.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ns-races',
   templateUrl: './races.component.html',
-  styleUrls: ['./races.component.css']
+  styleUrls: ['./races.component.css'],
 })
 export class RacesComponent implements OnInit {
+  races: any = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  refreshRacesList(event: Event) {
+    console.log('Refreshing races list...');
+    console.log(event);
+    this.races = [{ name: 'London' }, { name: 'Lyon' }];
   }
 
+  ngOnInit(): void {}
 }
